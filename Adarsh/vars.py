9e58db160d444a6ba6ce7bfea3771f685b13d88e
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2022-11-30 15:39:16 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -21,8 +21,9 @@ class Var(object):
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5175000602").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
-    APP_NAME = None
+    APP_NAME = file-to-stream
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', "rushidhar1999"))
+    ENV = True
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
